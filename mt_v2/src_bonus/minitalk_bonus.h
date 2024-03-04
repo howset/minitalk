bca782e8d60586cbf34b1e9ff71c86cf46134a9b
howset <36301071+howset@users.noreply.github.com>
2024-03-04 12:52:44 +0100

    Update minitalk_bonus.h

--- mt_v2/src_bonus/minitalk_bonus.h
+++ mt_v2/src_bonus/minitalk_bonus.h
@@ -12,11 +12,11 @@
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
-//# define _XOPEN_SOURCE 700 // sigaction&friends, in conjunction with signal.h
+# define _XOPEN_SOURCE 700 // sigaction&friends, in conjunction with signal.h
 //# define _DEFAULT_SOURCE // to avoid implicit declaration of usleep
 
 # include <unistd.h>
-//# include <sys/types.h> // pid_t 
+# include <sys/types.h> // pid_t 
 # include <signal.h> // SIGUSR1/2 kill sigaction
 # include "../lib/src/ft_printf/ft_printf.h"
 # include "../lib/src/get_next_line/get_next_line.h"
